54f2c34040747af3725285f8365a9cf170b1aad7
薛召 <xuezhao111@126.com>
2022-09-27 16:30:13 +0800

    ERC4907Upgradeable

---
+++ contracts/mocks/ERC4907MockUpgradeable.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0; 
+
+import "../token/ERC4907/ERC4907Upgradeable.sol";
+
+contract ERC4907MockUpgradeable is ERC4907Upgradeable {
+
+    function initialize(
+        string memory name_,
+        string memory symbol_
+    ) public initializer {
+        __ERC721_init(name_, symbol_);
+    }
+    function mint(uint256 tokenId, address to) public {
+        _mint(to, tokenId);
+    }
+
+} 
+
---
+++ test/token/ERC4907/ERC4907.test.js
@@ -0,0 +1,17 @@
+const {
+    shouldBehaveLikeERC4907
+} = require('./ERC4907.behavior');
+
+const ERC4907MockUpgradeable = artifacts.require('ERC4907MockUpgradeable');
+
+contract('ERC4907', function (accounts) {
+    const name = 'Non Fungible Token 4907';
+    const symbol = 'NFT4907';
+  
+    beforeEach(async function () {
+      this.token = await ERC4907MockUpgradeable.new(name, symbol);
+    });
+  
+    shouldBehaveLikeERC4907('ERC4907', ...accounts);
+  });
+
